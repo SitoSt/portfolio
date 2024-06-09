2837c1ee95d58c019ad769472897dd26ca6a4cf3
Sito <alfonsogarrelopez@gmail.com>
2024-06-09 02:51:13 +0200

    Responsive Header

--- src/views/HeaderView.tsx
+++ src/views/HeaderView.tsx
@@ -1,23 +1,46 @@
 import './css/header.css'
+import { useRef } from 'react'
 
 export const HeaderView = () => {
+
+    const menu = useRef<HTMLDivElement | null>(null)
+
+    const toggleMenu = () => {
+        if (menu.current) {
+            menu.current.classList.toggle('active')
+        }
+    }
+
+    const onClickLink = () => {
+        if (menu.current) {
+            if (menu.current.classList.contains('active')) {
+                menu.current.classList.toggle('active')
+            }
+        }
+    }
+
     return (
         <div className='header'>
             <img src="/imgs/svg/logo_Alfonso_Garre.svg" alt="logo Alfonso" className='logo' />
-            <nav className='header-nav'>
+
+            <button className='menu-button' onClick={toggleMenu}>
+                <img src="/imgs/svg/menu.svg" alt="Desplegar menu" />
+            </button>
+
+            <nav className='header-nav' ref={menu}>
 
                 <ul className='nav-list'>
 
                     <li className='nav-item'>
-                        <a className='nav-link' href='/'>Presentación</a>
+                        <a className='nav-link' href='/' onClick={onClickLink}>Presentación</a>
                     </li>
 
                     <li className='nav-item'>
-                        <a className='nav-link' href="#projects">Proyectos</a>
+                        <a className='nav-link' href="#projects" onClick={onClickLink}>Proyectos</a>
                     </li>
 
                     <li className='nav-item'>
-                        <a className='nav-link' href="#contact">Contacto</a>
+                        <a className='nav-link' href="#contact" onClick={onClickLink}>Contacto</a>
                     </li>
 
                 </ul>
