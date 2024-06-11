6f18ef9891f0c69ba9d4f387e7195fab88ae469e
Sito <alfonsogarrelopez@gmail.com>
2024-06-11 18:45:20 +0200

    Escribiendo descripción proyectos

--- src/App.tsx
+++ src/App.tsx
@@ -37,7 +37,12 @@ const projectsData: ProjectModel[] = [
     name: "Dl YouTube",
     imgUrl: "/imgs/projects/preview_dlyoutube.webp",
     url: "dlyoutube.alfonsogarre.com",
-    resumen: ["Una web para descargar videos de YouTube. Fue un desafió desarrollarla en Flask y aprender a desplegarla."],
+    resumen: [
+      `Esta es una web que permite descargar videos y audios de YouTube, está escrita en Python usando las librerías de Flask
+       y pytube. Esta última ofrece un montón de opciones, como descargar playlists enteras, buscar videos por canal o nombre y más.`,
+      `Por ahora Dl YouTube solo puede descargar con la URL del video, pero estoy deseando mejorarlo y añadir el resto de 
+       funcionalidades que proporciona pytube`
+    ],
     skills: ['python', 'flask', 'docker'],
     GitHubUrl: 'https://github.com/SitoSt/Dl_YouTube'
   },
@@ -45,7 +50,12 @@ const projectsData: ProjectModel[] = [
     name: "Clima App",
     imgUrl: "/imgs/projects/preview_clima_app.webp",
     url: "clima.alfonsogarre.com",
-    resumen: ["Web simple que se conecta a la API de OpenWeather para mostrar el clima. Resulta una buena practica de manejo de API con NEXT"],
+    resumen: [
+      `Realmente lo interesante de este proyecto es su código. Utilizo una arquitectura basada en un contexto, un provider
+       y un reducer, para poder gestionar la geolocalización del usuario, las peticiones a las diferentes API y sobre todo 
+       para hacerlo más escalable.`,
+      `Me ha servido mucho a la hora de añadir nuevas funcionalidades (como el autocompletado) sin tener que hacer grandes
+       cambios en el código`],
     skills: ['next', 'react', 'typescript'],
     GitHubUrl: 'https://github.com/SitoSt/Clima'
   }
